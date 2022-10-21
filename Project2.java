4dcc4ba224fecf18dc1a86862293eacdddf0522e
ikenton <imkenton@cpp.edu>
2022-10-21 01:19:11 -0700

    set up main method

--- Project2.java
+++ Project2.java
@@ -2,10 +2,52 @@ import java.util.Scanner;
 
 public class Project2{
     public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int number;
+        int response = 0;
+        double b[], x[], y[]; 
+        double equations[][];
 
+        //ask for user input
+        System.out.println("Please enter the number of linear equations.");
+        number = kb.nextInt();
+        
+        while(number > 10 || number <= 1){
+            System.out.println("The number you have entered is out of bounds. Please enter a number that is more than 1 and less then or equal to 10");
+            number = kb.nextInt();
+            System.out.println(number);
+        }
+
+        
+        System.out.println("Please enter the amount of linear equations.");
+        
+        while(number > 10 || number <= 1){
+            System.out.println("The number you have entered is out of bounds. Please enter a number that is more than 1 and less then or equal to 10");
+            number = kb.nextInt();
+            System.out.println(number);
+        }
+
+        b  = new double[number];
+        
+        equations = new double[number][4]; //Aij
+        x = new double[number];
+        y = new double[number]; //not sure if i need this yet
+
+
+        do{
+            System.out.println("Press 1 to enter a file. Press 2 to enter the equation by hand");
+            response = kb.nextInt();
+        }while(response != 1 || response != 2);
+
+        //load the equations onto array A
+        
+        guassSeidel(equations, b, x); //error?
+        jacobi(equations, b, x);
+       
+        kb.close();
     }
 
-    private void gaussSeidel(){
+    public static void gaussSeidel(double[][] equations, double[] b, double[] x){
         /*GaussSeidel(A,b,x) 
          *  kmax = 100
          *  sigma = 10^-10
@@ -43,7 +85,7 @@ public class Project2{
         */
     }
 
-    private void jacobi(){
+    private void jacobi(double[][] equations, double[] b, double[] x){
         //psuedocode
         /*Jacobi(A,b,x) 
          *  kmax = 100
@@ -81,7 +123,7 @@ public class Project2{
         */
     }
 
-    public static void readFile(){
+    public static void loadCoef(int option, double array[][]){
 
     }
 }
